82e711c6d8dbbe3190df397a6f527bdc29092253
wsd0727 <724797236@qq.com>
2019-05-16 19:04:23 +0800

    issue

--- pages/gridTabs/intelligentDistribution/IntelligentDistribution.vue
+++ pages/gridTabs/intelligentDistribution/IntelligentDistribution.vue
@@ -3,7 +3,7 @@
 		<div class="message">
 			<div class="top">
 				<image src="../../../static/img/icon/weizhi.png"></image>
-				<picker mode="region" @change="chooseStartAddress" >
+				<picker mode="region" @change="chooseStartAddress">
 					<view class="picker">{{address.start || '请输入起始地'}}</view>
 					<!-- <input type="text" :value="address.start" placeholder="请输入起始地" disabled /> -->
 				</picker>
@@ -15,9 +15,9 @@
 			</div>
 			<div class="con">
 				<input type="text" :value="goodsDescriptionData" placeholder="货物名称" placeholder-style="color:#afafaf" @change="goodsDescription" />
-				<input type="number" :value="goodsWeightData" placeholder="kg" placeholder-style="color:#afafaf" @change="goodsWeight"/>
+				<input type="number" :value="goodsWeightData" placeholder="kg" placeholder-style="color:#afafaf" @change="goodsWeight" />
 				<input type="number" :value="goodsBulkData" placeholder="m3" placeholder-style="color:#afafaf" @change="goodsBulk" />
-				<input type="text" :value="goodsCommentData" placeholder="备注" placeholder-style="color:#afafaf" @change="goodsComment"/>
+				<input type="text" :value="goodsCommentData" placeholder="备注" placeholder-style="color:#afafaf" @change="goodsComment" />
 			</div>
 			<button type="primary" @click="submitMessage">确定</button>
 		</div>
@@ -28,154 +28,162 @@
 		<div class="wait" :style="styles">
 			<h1>正在拼命匹配中 ...</h1>
 			<cmd-progress percent="100" status="active" :show-info="false" stroke-width="15"></cmd-progress>
-			<button type="default" @click="cancellation"  >取消订单</button>
+			<button type="default" @click="cancellation">取消订单</button>
 		</div>
 	</view>
 </template>
 
 <script>
-	import {uniPopup} from "@/components/uni-popup/uni-popup.vue"
+	import {
+		uniPopup
+	} from "@/components/uni-popup/uni-popup.vue"
 	import cmdProgress from "@/components/cmd-progress/cmd-progress.vue"
 	export default {
-		components:{
+		components: {
 			uniPopup,
 			cmdProgress
 		},
-		mounted(){
-			let self=this
+		mounted() {
+			let self = this
 			self.nowTimes()
 		},
 		data() {
 			return {
 				orderNumber: '',
-				show:false,
-				styles:'display:none',
-				schedule:'80',
-				poputs:false,
-				nowTime:'',
-				orderType:'2',
-				goodsCommentData:'',
-				goodsBulkData:'',
-				goodsWeightData:'',
-				goodsDescriptionData:'',
-				address:{
-					start:'',
-					end:'',
-					startLongitude:'120.583190',
-					startLatitude:'31.298340',
-					endLongitude:'121.473700', 
-					endLatitude:'31.230370'
-					},
-				chooseStartAddressData:''
+				show: false,
+				styles: 'display:none',
+				schedule: '80',
+				poputs: false,
+				nowTime: '',
+				orderType: '2',
+				goodsCommentData: '',
+				goodsBulkData: '',
+				goodsWeightData: '',
+				goodsDescriptionData: '',
+				address: {
+					start: '',
+					end: '',
+					startLongitude: '120.583190',
+					startLatitude: '31.298340',
+					endLongitude: '121.473700',
+					endLatitude: '31.230370'
+				},
+				chooseStartAddressData: ''
 			};
 		},
-		methods:{
+		methods: {
 			// 货主点击取消订单
-			cancellation(e){
-				let self=this
-				self.styles="display:none"
+			cancellation(e) {
+				let self = this
+				self.styles = "display:none"
+				let ordernumber=self.orderNumber
 				self.$request.get({
-					url:`/orderLogistics/cancelorder/${ordernumber}`
+					url: `/orderLogistics/cancelorder/${ordernumber}`
 				})
 			},
 			// 货主单击弹出层邀请
-			satisfaction(e){
+			satisfaction(e) {
 				uni.navigateTo({
-					url:'../cartlist/cartlist'
+					url: '../cartlist/cartlist'
 				})
 			},
 			// 货主单击弹出层取消
-			nosatisfaction(e){
-				let self=this
-				self.poputs=false
+			nosatisfaction(e) {
+				let self = this
+				self.poputs = false
 			},
 			// 获取当前时间
 			timeFormate(timeStamp) {
-				let self=this
-			    let year = new Date(timeStamp).getFullYear();
-			    let month =new Date(timeStamp).getMonth() + 1 < 10? "0" + (new Date(timeStamp).getMonth() + 1): new Date(timeStamp).getMonth() + 1;
-			    let date =new Date(timeStamp).getDate() < 10? "0" + new Date(timeStamp).getDate(): new Date(timeStamp).getDate();
-			    let hh =new Date(timeStamp).getHours() < 10? "0" + new Date(timeStamp).getHours(): new Date(timeStamp).getHours();
-			    let mm =new Date(timeStamp).getMinutes() < 10? "0" + new Date(timeStamp).getMinutes(): new Date(timeStamp).getMinutes();
-			    let ss=new Date(timeStamp).getSeconds() < 10? "0" + new Date(timeStamp).getSeconds(): new Date(timeStamp).getSeconds();
-			    self.nowTime =year+"-"+month+"-"+date+" "+hh+":"+mm+":"+ss;
+				let self = this
+				let newDate = new Date(timeStamp)
+				let month = newDate.getMonth()
+				let date = newDate.getDate()
+				let hh = newDate.getHours()
+				let mm = newDate.getMinutes()
+				let ss = newDate.getSeconds()
+				let year = new Date(timeStamp).getFullYear();
+				month = month + 1 < 10 ? "0" + (month + 1) : month + 1;
+				date = date < 10 ? "0" + date : date;
+				hh = hh < 10 ? "0" + hh : hh;
+				mm = mm < 10 ? "0" +mm : mm;
+				ss = ss < 10 ? "0" + ss : ss
+				self.nowTime = year + "-" + month + "-" + date + " " + hh + ":" + mm + ":" + ss;
 			},
 			//定时器
-			nowTimes(){
-				let self=this
+			nowTimes() {
+				let self = this
 				self.timeFormate(new Date());
-				setInterval(self.nowTimes,30*1000);
+				setInterval(self.nowTimes, 30 * 1000);
 			},
 			//单击确定按钮
-			submitMessage(e){
-				let self=this
-				let p=self.$request.post({
-					url:"/orderLogistics/add",
-					data:{
-						cargoName:self.goodsDescriptionData,
-						cargoVolume:self.goodsBulkData,
-						cargoWeight:self.goodsWeightData,
-						remark:self.goodsCommentData,
-						startLongitude:self.address.startLongitude,
-						startLatitude:self.address.startLatitude,
-						endLongitude:self.address.endLongitude,
-						endLatitude:self.address.endLatitude,
-						orderType:'2',
-						loadDate:self.nowTime
+			submitMessage(e) {
+				let self = this
+				let p = self.$request.post({
+					url: "/orderLogistics/add",
+					data: {
+						cargoName: self.goodsDescriptionData,
+						cargoVolume: self.goodsBulkData,
+						cargoWeight: self.goodsWeightData,
+						remark: self.goodsCommentData,
+						startLongitude: self.address.startLongitude,
+						startLatitude: self.address.startLatitude,
+						endLongitude: self.address.endLongitude,
+						endLatitude: self.address.endLatitude,
+						orderType: '2',
+						loadDate: self.nowTime
 					}
 				})
-				p.then(res=>{
-					console.log(res)
-					let self=this
-					// self.orderNumber = res.
-					if(res.matchOrderBeans && res.matchOrderBeans.length > 0){
-						console.log(res.matchOrderBeans)
-						self.styles="display:block"
-					}else{
+				p.then(res => {
+					console.log(self)
+					self.orderNumber = res.matchOrderBean.orderNumber
+					if (res.matchOrderBean.vehicleList && res.matchOrderBean.vehicleList.length > 0) {
+						self.styles = "display:block"
+					} else {
 						uni.showToast({
 							title: '没有匹配到车辆,请稍后重试',
 							icon: 'none',
 							duration: 2500
 						})
 					}
-					
-					
+
+
 				})
 			},
-			goodsComment(e){
-				let self=this
-				self.goodsCommentData=e.detail.value
+			goodsComment(e) {
+				let self = this
+				self.goodsCommentData = e.detail.value
 			},
-			goodsBulk(e){
-				let self=this
-				self.goodsBulkData=e.detail.value
+			goodsBulk(e) {
+				let self = this
+				self.goodsBulkData = e.detail.value
 			},
-			goodsWeight(e){
-				let self=this
-				self.goodsWeightData=e.detail.value
+			goodsWeight(e) {
+				let self = this
+				self.goodsWeightData = e.detail.value
 			},
-			goodsDescription(e){
-				let self=this
-				self.goodsDescriptionData=e.detail.value
+			goodsDescription(e) {
+				let self = this
+				self.goodsDescriptionData = e.detail.value
 			},
-			chooseStartAddress(e){
+			chooseStartAddress(e) {
 				let self = this
 				let address = e.detail.value
 				self.address.start = address[1]
 			},
-			chooseEndAddress(e){
-				let self=this
-				let address=e.detail.value
-				self.address.end=address[1]
+			chooseEndAddress(e) {
+				let self = this
+				let address = e.detail.value
+				self.address.end = address[1]
 			}
 		}
 	}
 </script>
 
 <style lang="scss" scoped>
-	#app{
+	#app {
 		width: 100%;
-		.message{
+
+		.message {
 			width: 690upx;
 			height: 234upx;
 			background-color: #fff;
@@ -183,46 +191,54 @@
 			margin: 10upx auto;
 			display: block;
 			padding: 20upx;
-			.top{
+
+			.top {
 				display: flex;
 				justify-content: space-around;
 				align-items: center;
-				picker{
+
+				picker {
 					color: #333;
 					font-size: 26upx;
 				}
-				image{
+
+				image {
 					width: 26upx;
 					height: 32upx;
 					margin-right: 10upx;
 				}
-				.replace{
+
+				.replace {
 					width: 42upx;
 					height: 42upx;
 				}
 			}
-			.con{
+
+			.con {
 				display: flex;
 				margin: 24upx 0;
 				font-size: 24upx;
-				input{
+
+				input {
 					border-bottom: #afafaf 1upx solid;
 					margin-right: 20upx;
 				}
 			}
-			button{
+
+			button {
 				width: 320upx;
 				height: 70upx;
 				line-height: 70upx;
 				border-radius: 50upx;
-				
+
 			}
 		}
-		.wait{
+
+		.wait {
 			width: 690upx;
 			height: 300upx;
 			back: #fff;
-			position:fixed;
+			position: fixed;
 			left: 0;
 			right: 0;
 			bottom: 0;
@@ -230,13 +246,15 @@
 			z-index: 22;
 			box-shadow: 0 0 6upx .2upx #666;
 			padding: 30upx 40upx 0;
-			h1{
+
+			h1 {
 				font-size: 34upx;
 				color: #333;
 				text-align: center;
 				margin-bottom: 10upx;
 			}
-			button{
+
+			button {
 				height: 120upx;
 				width: 100%;
 				position: fixed;
@@ -245,11 +263,4 @@
 			}
 		}
 	}
-	
-	
-	
-	
-	
-	
-
 </style>
--- pages/tabbar/issue/issue.vue
+++ pages/tabbar/issue/issue.vue
@@ -4,35 +4,53 @@
 			<div class="top">
 				<van-row>
 					<van-col :span="12">*装货地</van-col>
-					<van-col :span="12"><view class="picker">{{address.start || '选择省|市|区'}}</view></van-col>
+					<van-col :span="12">
+						<picker mode="region" @change="chooseStartCity" >
+							<view class="picker" >{{startAreaCode || '选择省 | 市 | 区'}}</view>
+						</picker>
+					</van-col>
 				</van-row>
 				<van-row>
 					<van-col :span="24">
-						<input type="text" placeholder="填写详细地址可帮您更快的找到司机">
+						<input type="text" placeholder="填写详细地址可帮您更快的找到司机" :value="startAddr" @change="chooseStartAddr">
 					</van-col>
 				</van-row>
 				<van-row>
 					<van-col :span="12">装货时间</van-col>
-					<van-col :span="12">
-						<picker mode="region" @change="chooseStartTime" >
-							<view class="picker">{{times.start || '请选择'}}</view>
+					<van-col :span="6">
+						<picker mode="date" @change="chooseLoadDate" >
+							<view class="date">{{loadDate || '年月日'}}</view>
+						</picker>
+					</van-col>
+					<van-col :span="6">
+						<picker mode="time" @change="chooseLoadTime" >
+							<view class="date">{{loadTime || '时分'}}</view>
 						</picker>
 					</van-col>
 				</van-row>
 				<van-row>
 					<van-col :span="12">*卸货地</van-col>
-					<van-col :span="12"><view class="picker">{{address.start || '选择省|市|区'}}</view></van-col>
-				</van-row>
-				<van-row>
-					<van-col :span="24">
-						<input type="text" placeholder="填写详细地址可帮您更快的找到司机">
+					<van-col :span="12">
+						<picker mode="region" @change="chooseEndCity" >
+							<view class="picker" >{{endAreaCode || '选择省 | 市 | 区'}}</view>
+						</picker>
 					</van-col>
 				</van-row>
 				<van-row>
-					<van-col :span="12">装货时间</van-col>
-					<van-col :span="12">
-						<picker mode="region" @change="chooseStartTime" >
-							<view class="picker">{{times.start || '请选择'}}</view>
+					<van-col :span="24">
+						<input type="text" placeholder="填写详细地址可帮您更快的找到司机" :value="endAddr" @change="chooseEndAddr">
+					</van-col>
+				</van-row>
+				<van-row>
+					<van-col :span="12">卸货时间</van-col>
+					<van-col :span="6">
+						<picker mode="date" @change="chooseDischargeDate" >
+							<view class="date">{{dischargeDate || '年月日'}}</view>
+						</picker>
+					</van-col>
+					<van-col :span="6">
+						<picker mode="time" @change="chooseDischargeTime" >
+							<view class="date">{{dischargeTime || '时分'}}</view>
 						</picker>
 					</van-col>
 				</van-row>
@@ -40,134 +58,291 @@
 		</div>
 		<div class="baseInfor">基础信息</div>
 		<div class="message">
-			<var-row>
+			<van-row>
 				<van-col :span="12">*选择车型</van-col>
 				<van-col :span="12">
-					<picker mode="region" @change="chooseCart" >
-						<view class="picker">{{carts || '请选择车型'}}</view>
+					 <picker @change="chooseVehicleType" :range="carts" >
+					   <view class="date">{{vehicleType || '请选择'}}</view>
 					</picker>
 				</van-col>
-			</var-row>
-			<var-row>
+			</van-row>
+			<van-row>
 				<van-col :span="12">*货物重量</van-col>
 				<van-col :span="12">
-					<input type="text">千克
+					<input type="text" :value="cargoWeight" @change="chooseCargoWeight">千克
 				</van-col>
-			</var-row>
-			<var-row>
+			</van-row>
+			<van-row>
 				<van-col :span="12">*货物体积</van-col>
 				<van-col :span="12">
-					<input type="text">m3
+					<input type="text" :value="cargoVolume" @change="chooseCargoVolume">m3
 				</van-col>
-			</var-row>
+			</van-row>
 		</div>
 		<div class="baseInfor">货物信息</div>
 		<div class="cargo">
 			<van-row>
 				<van-col :span="12">*货物名称</van-col>
-				<van-col :span="12"><input type="text" placeholder="必填货物名称" ></van-col>
-			</van-row>
-			<van-row>
-				<van-col :span="12">*包装方式</van-col>
-				<van-col :span="12"><view class="picker">{{address.start || '请选择'}}</view></van-col>
+				<van-col :span="12"><input type="text" placeholder="必填货物名称" :value="cargoName" @change="chooseCargoName"></van-col>
 			</van-row>
 		</div>
 		<div class="baseInfor">装卸信息</div>
 		<div class="handling">
 			<van-row>
-				<van-col :span="12">*几装几卸</van-col>
-				<van-col :span="12"><view class="picker">{{address.start || '请选择'}}</view></van-col>
-			</van-row>
-			<van-row>
-				<van-col :span="12">*装货时间</van-col>
-				<van-col :span="12"><view class="picker">{{address.start || '请选择'}}</view></van-col>
-			</van-row>
-			<van-row>
-				<van-col :span="12">*卸货时间</van-col>
-				<van-col :span="12"><view class="picker">{{address.start || '请选择'}}</view></van-col>
+				<van-col :span="12">*装卸方式</van-col>
+				<van-col :span="12">
+					<picker @change="chooseLoadNumAndDischargeNum" :range="handlingList" >
+					   <view class="date">{{loadNumAndDischargeNum || '请选择'}}</view>
+					</picker>
+				</van-col>
 			</van-row>
 		</div>
 		<div class="baseInfor">支付信息</div>
 		<div class="pay">
 			<van-row>
-				<van-col :span="12">*卸货时间</van-col>
-				<van-col :span="12"><view class="picker">{{address.start || '请选择'}}</view></van-col>
+				<van-col :span="12">*预期运价</van-col>
+				<van-col :span="12"><input type="text" :value="expectFreightRate" @change="chooseExpectFreightRate" placeholder="请合理填写运费 元/趟" /></van-col>
 			</van-row>
 			<van-row>
-				<van-col :span="12">*卸货时间</van-col>
-				<van-col :span="12"><view class="picker">{{address.start || '请选择'}}</view></van-col>
+				<van-col :span="12">*支付方式</van-col>
+				<van-col :span="12">
+					<picker @change="choosePayMode" :range="money">
+					   <view class="date">{{payMode || '请选择'}}</view>
+					</picker>
+				</van-col>
 			</van-row>
-			
-			
 		</div>
-		
+		<div class="baseInfor">其他</div>
+		<div class="comment">
+			<van-row>
+				<van-col :span="12">回单</van-col>
+				<van-col :span="12">
+					<!-- <van-radio-group v-model="radio">
+					  <van-radio name="1">单选框 1</van-radio>
+					  <van-radio name="2">单选框 2</van-radio>
+					</van-radio-group> -->
+				</van-col>
+			</van-row>
+			<van-row>
+				<van-col :span="12">备注</van-col>
+				<van-col :span="12"><input type="text" placeholder="请输入其他要求" :value="remark" @change="chooseRemark"> </van-col>
+			</van-row>
+		</div>
+		<button type="primary" @click="issueList">发布</button>
 		
 	</view>
 </template>
 
 <script>
 	export default {
+		mounted(){
+			let self=this
+			// 车型
+			self.$request.get({
+				url:'/getDictionary/transportType'
+			}).then((res)=>{
+				self.carts=[]
+				res.childrenDictionaryList.forEach(element => {
+				    self.carts.push(element.name)
+				});
+			})
+			//支付方式
+			self.$request.get({
+				url:'/getDictionary/paymentMethod%20'
+			}).then(res=>{
+				res.childrenDictionaryList.forEach(element=>{
+					self.money.push(element.name)
+				})
+			})
+			// 装卸方式
+			self.$request.get({
+				url:'/getDictionary/loadAndDischarge'
+			}).then(res=>{
+				res.childrenDictionaryList.forEach(element=>{
+					self.handlingList.push(element.name)
+				})
+			})
+			
+		},
 		data() {
 			return {
-				
-				carts:'',
-				times:{
-					start:'',
-					end:''
-				},
-				address:{
-				  "cargoName": "货物名称",
-				  "cargoVolume": "30",
-				  "cargoWeight": "55.6",
-				  "deposit": "200",
-				  "dischargeDate": "2019-05-15 19:14:24",
-				  "endAddr": "卸货地详细地址",
-				  "endAreaCode": "",
-				  "endCityCode": "",
-				  "endLatitude": "31.3017283408",
-				  "endLongitude": "120.5771827698",
-				  "endProvinceCode": "",
-				  "expectFreightRate": "",
-				  "isOverspeedOvermanTransfinite": "0",
-				  "isRegularCargoOrigin": "1",
-				  "loadDate": "2019-05-18 19:14:24",
-				  "loadNumAndDischargeNum": "1",
-				  "payMode": "1",
-				  "receipt": "0",
-				  "remark": "备注",
-				  "specificRequirement": "特殊要求",
-				  "startAddr": "装货地详细地址",
-				  "startAreaCode": "",
-				  "startCityCode": "",
-				  "startLatitude": "31.2311513192",
-				  "startLongitude": "121.4627838344",
-				  "startProvinceCode": "",
-				  "vehicleSize": "124",
-				  "vehicleType": "1"
-				},
+				billing:[],
+				handlingList:[],
+				money:[],
+				carts:['小汽车', '大卡车'],
+				cargoName: "",
+				cargoVolume: "",
+				cargoWeight: "",
+				//最终卸货之间
+				lastdischargeDate:"",
+				dischargeTime:"",
+				dischargeDate: "",
+				endAddr: "",
+				endAreaCode: "",
+				endCityCode: "",
+				endLatitude: "31.3017283408",
+				endLongitude: "120.5771827698",
+				endProvinceCode: "",
+				expectFreightRate: "",
+				isOverspeedOvermanTransfinite: "0",
+				isRegularCargoOrigin: "1",
+				//最终装货时间
+				lastLoadDate:"",
+				loadTime:"",
+				loadDate: "",
+				loadNumAndDischargeNum: "1",
+				payMode: "",
+				receipt: "0",
+				remark: "",
+				specificRequirement: "",
+				startAddr: "",
+				startAreaCode: "",
+				startCityCode: "",
+				startLatitude: "31.2311513192",
+				startLongitude: "121.4627838344",
+				startProvinceCode: "",
+				vehicleSize: "124",
+				vehicleType: ""
 			}
 		},
-		onLoad() {
-
-		},
 		methods: {
-			chooseEndTime(e){
-				
+			// 单击发布按钮
+			issueList(e){
+				let self=this
+				console.log(e)
+				self.$request.post({
+					url:'/logistics/shipperGoodsReleased/add',
+					data:{
+						cargoName:self.cargoName,
+						cargoVolume:self.cargoVolume,
+						cargoWeight:self.cargoWeight,
+						dischargeDate: "卸货时间(yyyy-MM-dd HH:mm:ss)",
+						endAddr:self.endAddr,
+						endAreaCode:self.endAreaCode,
+						endCityCode:self.endCityCode,
+						endProvinceCode:self.endProvinceCode,
+						  "endLatitude": "*目的定位纬度",
+						  "endLongitude": "*目的定位经度",
+						expectFreightRate:self.expectFreightRate,
+						  "id": "编号(新增为null 修改时传值)",
+						  "isOverspeedOvermanTransfinite": "是否三超(0 否 1 是)",
+						  "isRegularCargoOrigin": "是否常发货源(0 否 1 是)",
+						  "loadDate": "装货时间(yyyy-MM-dd HH:mm:ss)",
+						  "loadNumAndDischargeNum": "几装几卸",
+						  "payMode": "支付方式",
+						  "receipt": "是否需要回单(0 否 1 是)",
+						  "remark": "备注",
+						  "specificRequirement": "特殊要求",
+						  "startAddr": "装货地详细地址",
+						  "startAreaCode": "*始发区",
+						  "startCityCode": "*始发市",
+						  "startLatitude": "*始发定位纬度",
+						  "startLongitude": "始发定位经度",
+						  "startProvinceCode": "*始发省",
+						  "vehicleSize": "车长",
+						  "vehicleType": "车型"
+					}
+				})
 			},
-			chooseEndAddress(e){
-				let self = this
-				let address = e.detail.value
-				self.address.end = address[1]
+			// 装卸方式
+			chooseLoadNumAndDischargeNum(e){
+				let self=this
+				let loadNumAndDischargeNum=e.detail.value
+				self.loadNumAndDischargeNum=self.handlingList[loadNumAndDischargeNum]
 			},
-			// 选择开始时间
-			chooseStartTime(e){
-				
+			// 备注
+			chooseRemark(e){
+				let self=this
+				let remark=e.detail.value
+				self.remark=remark
 			},
-			//选择开始地址
-			chooseStartAddress(e){
-				let self = this
-				let address = e.detail.value
-				self.address.start = address[1]
+			// 支付方式
+			choosePayMode(e){
+				let self=this
+				let payMode=e.detail.value
+				self.payMode=self.money[payMode]
+			},
+			// 预期运价
+			chooseExpectFreightRate(e){
+				let self=this
+				let expectFreightRate=e.detail.value
+				self.expectFreightRate=expectFreightRate
+			},
+			// 货物名称
+			chooseCargoName(e){
+				let self=this
+				let cargoName=e.detail.value
+				self.cargoName=cargoName
+			},
+			// 货物体积
+			chooseCargoVolume(e){
+				let self=this
+				let cargoVolume=e.detail.value
+				self.cargoVolume=cargoVolume
+			},
+			// 货物重量
+			chooseCargoWeight(e){
+				let self=this
+				let cargoWeight=e.detail.value
+				self.cargoWeight=cargoWeight
+			},
+			// 车型
+			chooseVehicleType(e){
+				let self=this
+				let vehicleType=e.detail.value
+				self.vehicleType=this.carts[vehicleType]
+			},
+			//卸货时间时分
+			chooseDischargeTime(e){
+				let self=this
+				let dischargeTime=e.detail.value
+				self.dischargeTime=dischargeTime
+			},
+			// 卸货时间年月日
+			chooseDischargeDate(e){
+				let self=this
+				let dischargeDate=e.detail.value
+				self.dischargeDate=dischargeDate
+			},
+			// 卸货详细地址
+			chooseEndAddr(e){
+				let self=this
+				let endAddr=e.detail.value
+				self.endAddr=endAddr
+			},
+			// 卸货地
+			chooseEndCity(e){
+				let self=this
+				let val=e.detail.value
+				self.endProvinceCode=val[0]
+				self.endCityCode=val[1]
+				self.endAreaCode=val[2]
+			},
+			//装货时间时分
+			chooseLoadTime(e){
+				let self=this
+				let loadTime=e.detail.value
+				self.loadTime=loadTime
+			},
+			// 装货时间年月日
+			chooseLoadDate(e){
+				let self=this
+				let loadDate=e.detail.value
+				self.loadDate=loadDate
+			},
+			// 装货详细开始地址
+			chooseStartAddr(e){
+				let self=this
+				let startAddr=e.detail.value
+				self.startAddr=startAddr
+			},
+			// 选择装货城市
+			chooseStartCity(e){
+				let self=this
+				let val=e.detail.value
+				self.startProvinceCode=val[0]
+				self.startCityCode=val[1]
+				self.startAreaCode=val[2]
 			}
 		}
 	}
@@ -190,7 +365,7 @@
 			background: #f2f2f2;
 			border-left:2upx solid lightgreen;
 		}
-		.message,.cargo,.handling{
+		.message,.cargo,.handling,.pay,.comment{
 			background-color: #fff;
 			padding: 30upx 30upx 0;
 		}

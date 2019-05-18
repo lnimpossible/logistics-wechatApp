8594947c08d4389fe6e87eff82a273e3c03890b2
wsd0727 <724797236@qq.com>
2019-05-18 17:36:21 +0800

    five

--- pages/redictUrl/ordergoods/ordergoods.vue
+++ pages/redictUrl/ordergoods/ordergoods.vue
@@ -1,30 +1,12 @@
 <template>
 	<view class="content">
 		<ul class="tabs">
-			<li class="tabsLi" :class="{'active': index == currentLiItem? true:false}" v-for="(statusList,index) in statusList" :key="index" @click="toggleTabs(index)" >
+			<li class="tabsLi" v-for="(statusList,index) in statusList" :key="index" @click="toggleTabs(index)" 
+			:class="{active:index!=nowIndex}">
 				{{statusList}}
 			</li>
 		</ul>
-		<div class="tabson">
-			<div class="matter" v-for="(order,index) in orderList" :key="index">
-				<van-row>
-					<van-col :span="10">{{order.startCity}}</van-col>
-					<van-col :span="4">
-						<image mode="aspectFit" src="../../../static/img/icon/chongzhi.png"></image>
-					</van-col>
-					<van-col :span="10">{{order.endCity}}</van-col>
-				</van-row>
-				<van-row>
-					<van-col :span="6">货物重量:</van-col>
-					<van-col :span="6">{{order.cargoWeight}}kg</van-col>
-					<van-col :span="6">货物体积: </van-col>
-					<van-col :span="6">{{order.cargoVolume}}m3</van-col>
-				</van-row>
-				<van-row>
-					<van-col :span="24">订单号:{{order.orderNumber}}</van-col>
-				</van-row>
-			</div>
-		</div>
+		<div class="tabson" v-show="current===0">
 			<div class="matter"   v-for="(order,index) in orderList" :key="index">
 				<van-row>
 					<van-col :span="10">{{order.startCity}}</van-col>
@@ -43,6 +25,107 @@
 					<van-col :span="24">订单号:{{order.orderNumber}}</van-col>
 				</van-row>
 			</div>
+		</div>
+		<div class="tabson" v-show="current===1">
+			<div class="matter"   v-for="(order,index) in orderList" :key="index">
+				<van-row>
+					<van-col :span="10">{{order.startCity}}</van-col>
+					<van-col :span="4">
+						<image src="../../../static/img/icon/chongzhi.png"></image>
+					</van-col>
+					<van-col :span="10">{{order.endCity}}</van-col>
+				</van-row>
+				<van-row>
+					<van-col :span="6">货物重量:</van-col>
+					<van-col :span="6">{{order.cargoWeight}}kg</van-col>
+					<van-col :span="6">货物体积: </van-col>
+					<van-col :span="6">{{order.cargoVolume}}m3</van-col>
+				</van-row>
+				<van-row>
+					<van-col :span="24">订单号:{{order.orderNumber}}</van-col>
+				</van-row>
+			</div>
+		</div>
+		<div class="tabson" v-show="current===2">
+			<div class="matter"   v-for="(order,index) in orderList" :key="index">
+				<van-row>
+					<van-col :span="10">{{order.startCity}}</van-col>
+					<van-col :span="4">
+						<image src="../../../static/img/icon/chongzhi.png"></image>
+					</van-col>
+					<van-col :span="10">{{order.endCity}}</van-col>
+				</van-row>
+				<van-row>
+					<van-col :span="6">货物重量:</van-col>
+					<van-col :span="6">{{order.cargoWeight}}kg</van-col>
+					<van-col :span="6">货物体积: </van-col>
+					<van-col :span="6">{{order.cargoVolume}}m3</van-col>
+				</van-row>
+				<van-row>
+					<van-col :span="24">订单号:{{order.orderNumber}}</van-col>
+				</van-row>
+			</div>
+		</div>
+		<div class="tabson" v-show="current===3">
+			<div class="matter"   v-for="(order,index) in orderList" :key="index">
+				<van-row>
+					<van-col :span="10">{{order.startCity}}</van-col>
+					<van-col :span="4">
+						<image src="../../../static/img/icon/chongzhi.png"></image>
+					</van-col>
+					<van-col :span="10">{{order.endCity}}</van-col>
+				</van-row>
+				<van-row>
+					<van-col :span="6">货物重量:</van-col>
+					<van-col :span="6">{{order.cargoWeight}}kg</van-col>
+					<van-col :span="6">货物体积: </van-col>
+					<van-col :span="6">{{order.cargoVolume}}m3</van-col>
+				</van-row>
+				<van-row>
+					<van-col :span="24">订单号:{{order.orderNumber}}</van-col>
+				</van-row>
+			</div>
+		</div>
+		<div class="tabson" v-show="current===4">
+			<div class="matter"   v-for="(order,index) in orderList" :key="index">
+				<van-row>
+					<van-col :span="10">{{order.startCity}}</van-col>
+					<van-col :span="4">
+						<image src="../../../static/img/icon/chongzhi.png"></image>
+					</van-col>
+					<van-col :span="10">{{order.endCity}}</van-col>
+				</van-row>
+				<van-row>
+					<van-col :span="6">货物重量:</van-col>
+					<van-col :span="6">{{order.cargoWeight}}kg</van-col>
+					<van-col :span="6">货物体积: </van-col>
+					<van-col :span="6">{{order.cargoVolume}}m3</van-col>
+				</van-row>
+				<van-row>
+					<van-col :span="24">订单号:{{order.orderNumber}}</van-col>
+				</van-row>
+			</div>
+		</div>
+		<div class="tabson" v-show="current===5">
+			<div class="matter"   v-for="(order,index) in orderList" :key="index">
+				<van-row>
+					<van-col :span="10">{{order.startCity}}</van-col>
+					<van-col :span="4">
+						<image src="../../../static/img/icon/chongzhi.png"></image>
+					</van-col>
+					<van-col :span="10">{{order.endCity}}</van-col>
+				</van-row>
+				<van-row>
+					<van-col :span="6">货物重量:</van-col>
+					<van-col :span="6">{{order.cargoWeight}}kg</van-col>
+					<van-col :span="6">货物体积: </van-col>
+					<van-col :span="6">{{order.cargoVolume}}m3</van-col>
+				</van-row>
+				<van-row>
+					<van-col :span="24">订单号:{{order.orderNumber}}</van-col>
+				</van-row>
+			</div>
+		</div>
 			<view class="chooseBotton" v-show="status.current === 0">
 				<div class="matter"   v-for="(order,index) in orderList" :key="index">
 					<van-row>
@@ -170,11 +253,12 @@
 <script>
 	export default {
 		mounted() {
-			//订单状态( orderStatus ：-2 全部 -1已取消 0 待接单 1 已接单 2 运输中 3 运输完成 4 已完成 )
+			//订单状态( orderStatus ：-1 全部 0 待接单 1 已接单 2 运输中 3 运输完成 4 已完成 )
 			let self = this
 			self.$request.get({
 				url: '/orderLogistics/shipperOrderList/-2',
 			}).then(res => {
+				console.log(res)
 				self.orderList=res.orderList
 			})
 		},
@@ -182,7 +266,6 @@
 			return {
 				// 货物
 				orderList:[],
-				currentLiItem: 0,
 				goods: {
 					endCity:'',
 					startCity:'',
@@ -192,13 +275,25 @@
 					startCityCode: '',
 					endCityCode: ''
 				},
-				statusList:['全部', '待接单', '已接单', '运输中', '运输完成', '已完成']
+				statusList:['全部', '待接单', '已接单', '运输中', '运输完成', '已完成'],
+				current:0,
+				orderStatus:'',
 			}
 		},
 		methods: {
 			toggleTabs(index){
 				let self=this
-				self.currentLiItem = index
+				console.log(index)
+				self.orderStatus=index
+				self.current=index
+				let sourceIndex = index === 0 ? '-2' : index - 1
+				self.getOrderList(sourceIndex)
+			},
+			//订单状态( orderStatus ：null 全部 0 待接单 1 已接单 2 运输中 3 运输完成 4 已完成 )
+			chooseStatus(index) {
+				let self = this
+				self.status.orderStatus = index
+				self.status.current = index
 				let sourceIndex = index === 0 ? '-2' : index - 1
 				self.getOrderList(sourceIndex)
 			},
@@ -207,6 +302,7 @@
 				self.$request.get({
 					url: '/orderLogistics/shipperOrderList/' + orderStatus,
 				}).then(res => {
+					console.log(res)
 					self.orderList=res.orderList
 				})
 			}
@@ -228,9 +324,6 @@
 				color: #333;
 				font-size: 26upx;
 			}
-			.tabsLi.active{
-				color: orangered;
-			}
 		}
 		.tabson{
 			background: #fff;
@@ -238,6 +331,7 @@
 			.matter{
 				padding:20upx 30upx;
 				border-radius: 20upx;
+				margin-bottom: 10upx;
 				van-row{
 					color: #333;
 					font-size: 24upx;
